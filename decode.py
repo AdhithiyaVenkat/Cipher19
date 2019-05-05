f77a1ecb95735940588602adce4c1853f0a1f091
COSMOS <cosmos@localhost.localdomain>
2019-05-05 22:53:37 +0530

    Cipher Codings Updated

---
+++ decode.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+alph = 'abcdefghijklmnopqrstuvwxyz'
+a = []
+char = input('Please enter a character: ')
+key = int(input('Enter a key(1-26): '))
+for i in char:
+  if(i==' '):
+    newC = ' '
+    a.append(newC)
+  else:
+    pos = alph.find(i)
+    NewPos = (pos - key) % 26
+    newC = alph[NewPos]
+    a.append(newC)
+msg = ''.join(a)
+print(msg)
